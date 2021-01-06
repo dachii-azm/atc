c1d5b378cf095463b5567b1b3865e8f903f34715
dachii-azm <alohadaichi@gmail.com>
2021-01-06 18:40:58 +0900

    abc043 AB

---
+++ abc/abc043/B.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+  string s;
+  cin>>s;
+
+  string ans = "";
+
+  for(int i=0; i<s.size(); i++){
+    if(s[i] == '0') ans+='0';
+    else if(s[i] == '1') ans+='1';
+    else if(s[i] == 'B'){
+      if(!ans.empty()){
+        ans.erase(ans.size()-1, 1);
+      }
+    }
+  }
+
+  cout<<ans<<endl;
+}
