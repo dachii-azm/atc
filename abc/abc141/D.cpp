6feffc2c5aa3fc7b725e6ac22d8268d111915286
Daichi Azuma <azuma.daichi.62z@st.kyoto-u.ac.jp>
2021-06-29 14:56:10 +0900

    abc141

--- abc/abc141/D.cpp
+++ abc/abc141/D.cpp
@@ -7,8 +7,28 @@ int main(){
 	long long int n, m;
 	cin>>n>>m;
 
-	long long int a[n];
-	for(int i=0; i<n; i++) cin>>a[i];
+	priority_queue<int> q;
+	long long int sum = 0;
+
+	for(int i=0; i<n; i++){
+		int num;
+		cin>>num;
+		q.push(num);
+		sum+=num;
+	}
+
+	while(m--){
+		int x=q.top();
+		q.pop();
+		
+		sum-=(x-x/2);
+		x/=2;
+		q.push(x);
+	}
+
+	cout<<sum<<endl;
+
+
 
 
 }
